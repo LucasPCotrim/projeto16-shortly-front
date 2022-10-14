c65230f8dbdc161869b51a01110768e28d44bb6f
LucasPCotrim <lucaspcotrim@gmail.com>
2022-10-13 22:49:55 -0300

    feat: Added GlobalStyle file

---
+++ src/global/GlobalStyle.js
@@ -0,0 +1,160 @@
+import { createGlobalStyle, css } from 'styled-components';
+
+const Reset = css`
+  html,
+  body,
+  div,
+  span,
+  applet,
+  object,
+  iframe,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  p,
+  blockquote,
+  pre,
+  a,
+  abbr,
+  acronym,
+  address,
+  big,
+  cite,
+  code,
+  del,
+  dfn,
+  em,
+  img,
+  ins,
+  kbd,
+  q,
+  s,
+  samp,
+  small,
+  strike,
+  strong,
+  sub,
+  sup,
+  tt,
+  var,
+  b,
+  u,
+  i,
+  center,
+  dl,
+  dt,
+  dd,
+  ol,
+  ul,
+  li,
+  fieldset,
+  form,
+  label,
+  legend,
+  table,
+  caption,
+  tbody,
+  tfoot,
+  thead,
+  tr,
+  th,
+  td,
+  article,
+  aside,
+  canvas,
+  details,
+  embed,
+  figure,
+  figcaption,
+  footer,
+  header,
+  hgroup,
+  menu,
+  nav,
+  output,
+  ruby,
+  section,
+  summary,
+  time,
+  mark,
+  audio,
+  video {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    font: inherit;
+    vertical-align: baseline;
+  }
+  /* HTML5 display-role reset for older browsers */
+  article,
+  aside,
+  details,
+  figcaption,
+  figure,
+  footer,
+  header,
+  hgroup,
+  menu,
+  nav,
+  section {
+    display: block;
+  }
+  body {
+    line-height: 1;
+  }
+  ol,
+  ul {
+    list-style: none;
+  }
+  blockquote,
+  q {
+    quotes: none;
+  }
+  blockquote:before,
+  blockquote:after,
+  q:before,
+  q:after {
+    content: '';
+    content: none;
+  }
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+  }
+`;
+
+const Global = css`
+  * {
+    box-sizing: border-box;
+    border: 0;
+    padding: 0;
+  }
+  body {
+    --primary-color: #f9f7f7;
+    --secundary-color: #dbe2ef;
+    --tertiary-color: #3f72af;
+    --quaternary-color: #112d4e;
+    /* https://colorhunt.co/palette/f9f7f7dbe2ef3f72af112d4e */
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-start;
+    overflow: hidden;
+    background-color: var(--quaternary-color);
+    color: var(--primary-color);
+    font-family: 'Lexend Deca', sans-serif;
+    font-weight: 400;
+  }
+`;
+
+const GlobalStyle = createGlobalStyle`
+  ${Reset};
+  ${Global};
+`;
+
+export default GlobalStyle;
