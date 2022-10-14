9b7ce3dd5e2ac0357918106a1c40b6cda7208500
LucasPCotrim <lucaspcotrim@gmail.com>
2022-10-13 22:50:42 -0300

    feat: Implemented overall project structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import GlobalStyle from './global/GlobalStyle';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 export default function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<h1>HomePage</h1>} />
+          <Route path='/sign-up' element={<h1>SignUpPage</h1>} />
+          <Route path='/login' element={<h1>LoginPage</h1>} />
+          <Route path='/ranking' element={<h1>RankingPage</h1>} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
