f2ac33bd4243df8ca3b3f1e82d15a86e21ecead6
LucasPCotrim <lucaspcotrim@gmail.com>
2022-10-16 01:06:14 -0300

    feat: Added functions to get user info on MainPage load and shorten URL on button click

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,8 +1,24 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { MainPageStyle } from './MainPage.style';
-import { useState } from 'react';
+import { useState, useContext, useEffect } from 'react';
+import UserContext from '../../contexts/UserContext';
+import { getUserInfo, shortenURL } from '../../services/shortlyService';
 
 export default function MainPage() {
+  const { user, setUser } = useContext(UserContext);
   const [urlToShorten, setUrlToShorten] = useState('');
+
+  useEffect(() => {
+    const promise = getUserInfo();
+    promise
+      .then((res) => {
+        setUser({ ...user, name: res.data.name, shortenedUrls: res.data.shortenedUrls });
+        console.log(user);
+      })
+      .catch((res) => {
+        alert(res.response?.data?.message || 'Error when connecting to the database');
+      });
+  }, []);
 
   const handleInput = (event) => {
     setUrlToShorten(event.target.value);
@@ -10,9 +26,16 @@ export default function MainPage() {
   const clearInput = () => {
     setUrlToShorten('');
   };
-  const submitUrl = (event) => {
+  const submitUrl = () => {
+    const promise = shortenURL({ url: urlToShorten });
+    promise
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((res) => {
+        alert(res.response?.data?.message || 'Error when connecting to the database');
+      });
     clearInput();
-    console.log(urlToShorten);
   };
 
   return (
